30d21643c5f5af56acb68de551c663ec388ff566
Kohyunho1 <zigzag7489@gmail.com>
2024-07-14 20:27:18 +0900

    count 해결

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,14 +8,13 @@ public class Main {
 		System.out.println("==프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
+		List<String> articles = new ArrayList<>();
 	
 		while (true) {
 			System.out.printf("명령어)");
 			
 			String cmd = sc.nextLine();
-			
-			int count = 1;
-			
+					
 			if(cmd.equals("article list")) {
 				System.out.println("게시글이 없습니다.");
 				
@@ -23,17 +24,15 @@ public class Main {
 				System.out.printf("내용 : ");
 				String cmd3 = sc.nextLine();
 				
-				for(int i=0; i<count; i++) {
-					System.out.println(count + "번 글이 생성되었습니다.");
-					count++;
-				}
+				int count = articles.size() +1;
+				String newCount = count + "번글이 생성되었습니다.";
+				articles.add(newCount);
+				System.out.println(newCount);
 				
 			} else if (cmd.equals("")) {
-				System.out.println("명령어를 입력해주세요.");
-				
+				System.out.println("명령어를 입력해주세요.");			
 			} else if (cmd.equals("exit")) {
-				break;
-				
+				break;				
 			} else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}			
---
+++ src/Study.java
@@ -0,0 +1,10 @@
+
+public class Study {
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 8;
+		
+		System.out.println("x : " + x + y);
+	}
+}
