ce3940ce67bead8de9cc5482a4d25a09f6c61150
Kohyunho1 <zigzag7489@gmail.com>
2024-07-24 00:07:36 +0900

    Controller

--- src/com/exam/BAM/controller/MemberController.java
+++ src/com/exam/BAM/controller/MemberController.java
@@ -7,19 +7,28 @@ import java.util.Scanner;
 import com.exam.BAM.dto.Member;
 import com.exam.BAM.util.Util;
 
-public class MemberController {
-	
-	private Scanner sc;
-	private int lastMemberId;
+public class MemberController extends Controller {
+
 	private List<Member> members;
 	
 	public MemberController(Scanner sc) {
 		this.sc = sc;
-		this.lastMemberId = 0;
+		this.lastId = 0;
 		this.members = new ArrayList<>();
 	}
 	
-	public void doJoin() {
+	@Override
+	public void doAction(String cmd, String methodName) {
+		switch (methodName) {
+		case "join":
+			doJoin();
+			break;
+		default:
+			System.out.println("존재하지 않는 명령어 입니다");
+		}
+	}
+	
+	private void doJoin() {
 		String loginId = null;
 		String loginPw = null;
 		String name = null;
@@ -29,7 +38,7 @@ public class MemberController {
 			loginId = sc.nextLine().trim();
 			
 			if (loginId.length() == 0) {
-				System.out.println("아이디는 필수 입력 정보입니다.");
+				System.out.println("아이디는 필수 입력 정보입니다");
 				continue;
 			}
 			
@@ -43,21 +52,20 @@ public class MemberController {
 			}
 			
 			if (isLoginIdDup) {
-				System.out.printf("[ %s ]은(는) 이미 사용중인 아이디입니다.\n", loginId);
+				System.out.printf("[ %s ]은(는) 이미 사용중인 아이디입니다\n", loginId);
 				continue;
 			}
 			
-			System.out.printf("[ %s ]은(는) 사용가능한 아이디입니다.\n", loginId);
+			System.out.printf("[ %s ]은(는) 사용가능한 아이디입니다\n", loginId);
 			break;
 		}
 		
 		while (true) {
-			
 			System.out.printf("비밀번호 : ");
 			loginPw = sc.nextLine();
 			
 			if (loginPw.length() == 0) {
-				System.out.println("비밀번호는 필수 입력 정보입니다.");
+				System.out.println("비밀번호는 필수 입력 정보입니다");
 				continue;
 			}
 			
@@ -65,7 +73,7 @@ public class MemberController {
 			String loginPwChk = sc.nextLine();
 			
 			if (loginPw.equals(loginPwChk) == false) {
-				System.out.println("비밀번호가 일치하지 않습니다.");
+				System.out.println("비밀번호가 일치하지 않습니다");
 				continue;
 			}
 			break;
@@ -75,26 +83,28 @@ public class MemberController {
 			System.out.printf("이름 : ");
 			name = sc.nextLine().trim();
 			
-			if (name.equals("")) {
-				System.out.println("이름은 필수 입력 정보입니다.");
+			if (name.length() == 0) {
+				System.out.println("이름은 필수 입력 정보입니다");
 				continue;
 			}
 			break;
 		}
-
-		lastMemberId++;
-
-		Member member = new Member(lastMemberId, Util.getDateStr(), loginId, loginPw, name);
-
+		
+		lastId++;
+		
+		Member member = new Member(lastId, Util.getDateStr(), loginId, loginPw, name);
+		
 		members.add(member);
-
-		System.out.println(loginId + "님이 가입되었습니다.");
+		
+		System.out.println(name + "님이 가입되었습니다");
 	}
-	
+
+	@Override
 	public void makeTestData() {
 		System.out.println("테스트용 회원 데이터 3개를 생성했습니다");
 		for (int i = 1; i <= 3; i++) {
-			members.add(new Member(++lastMemberId, Util.getDateStr(), "test" + i, "test" + i, "유저" + i));
+			members.add(new Member(++lastId, Util.getDateStr(), "test" + i, "test" + i, "유저" + i));
 		}
 	}
+	
 }
